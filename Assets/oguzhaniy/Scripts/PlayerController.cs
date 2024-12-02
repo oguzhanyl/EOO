5ec9e02a5ebbae9958b9012fc6a2db903424d02b
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2024-12-02 18:33:36 +0300

    charController-2
    Karaktere sağa sola yürüme, kamera ile serbest dolaşma, zıplama aynı zamanda yer çekimi ve leftshift ile hızlı koşma eklendi.

---
+++ Assets/oguzhaniy/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    private CharacterController controller;
+    [SerializeField] private Transform camera;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float sprintTransitSpeed = 5f;
+    [SerializeField] private float turningSpeed = 2f;
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float jumpHeight = 2f;
+
+    private float verticalVelocity;
+    private float speed;
+
+    [Header("Input")]
+    private float moveInput;
+    private float turnInput;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        InputManagement();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        GroundMovement();
+        Turn();
+    }
+
+    private void GroundMovement()
+    {
+        Vector3 move = new Vector3(turnInput, 0, moveInput);
+        move = camera.transform.TransformDirection(move);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = Mathf.Lerp(speed, sprintSpeed,sprintTransitSpeed * Time.deltaTime);
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, walkSpeed, sprintTransitSpeed * Time.deltaTime);
+        }
+
+        move *= speed;
+
+        move.y = VerticalForceCalculation();
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private void Turn()
+    {
+        if(Mathf.Abs(turnInput) > 0 || Mathf.Abs(moveInput) > 0)
+        {
+
+            Vector3 currentLookDirection = controller.velocity.normalized;
+            currentLookDirection.y = 0;
+
+            currentLookDirection.Normalize();
+
+            Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turningSpeed);
+        }
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        return verticalVelocity;
+    }
+
+    private void InputManagement()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+}
